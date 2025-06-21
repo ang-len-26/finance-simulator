625b5d5a832461047705cd52db119ea1c3b4ed08
Angel Lengua <renelengua26@gmail.com>
2025-06-21 17:41:24 -0500

    fix render

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/token/';
+const API_URL = `${process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"}/token/`;
 
 export const loginUser = async (credentials: { username: string; password: string }) => {
   const response = await axios.post(API_URL, credentials);
