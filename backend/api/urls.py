f6eb01d8f4c68dfc7e115b2af57b84f75ae73733
Angel Lengua <renelengua26@gmail.com>
2025-06-21 19:11:33 -0500

    Agrega endpoint temporal para ejecutar migrate

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,17 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TransactionViewSet
 
+# api/urls.py
+from .views import run_migrations
+
+urlpatterns = [
+    path('run-migrations/', run_migrations),
+]
+
 router = DefaultRouter()
 router.register(r'transactions', TransactionViewSet, basename='transaction')
 
 urlpatterns = [
     path('', include(router.urls)),
+	path('run-migrations/', run_migrations),
 ]
