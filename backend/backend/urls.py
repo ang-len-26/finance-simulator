14f4f2703300d62e81fc9b2a316a1c84c6e850cc
Angel Lengua <renelengua26@gmail.com>
2025-06-23 20:02:18 -0500

    fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import run_migrations
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -24,6 +25,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
 	path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+	path('api/run-migrations/', run_migrations),
 	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
